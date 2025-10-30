a8b14d6089daf29bb9f61e301319a2168cf7a832
Bruno Peixoto <brunolnetto@gmail.com>
2025-10-30 16:58:04 -0300

    refactor(): Reference artifacts in initializer

--- data/boty/boty/__init__.py
+++ data/boty/boty/__init__.py
@@ -1,9 +1,33 @@
 from .core import (
-    
-    Signal,
-    RiskManagement,
-    Backtester,
-    LiveTrader,
+    StateManager,
+)
+from .data_models import (
+    Ticker, Candle, Signal, Position, Trade, SignalAction,
+)
+from .connectors import (
+    ExchangeConnector,
+    BinanceConnector,
+)
+from 
+from .strategies import (
+    Strategy,
+    EMACrossoverStrategy,
+    RiskController,
+    RiskParams,
 )
 
-from .strategy import EMA_CrossoverStrategy
+__all__ = [
+    'StateManager',
+    'Ticker',
+    'Candle',
+    'Signal',
+    'Position',
+    'Trade',
+    'SignalAction',
+    'ExchangeConnector',
+    'BinanceConnector',
+    'Strategy',
+    'EMACrossoverStrategy',
+    'RiskController',
+    'RiskParams',
+]
