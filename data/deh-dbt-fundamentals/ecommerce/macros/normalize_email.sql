88ac26595d93b05a02e61fdbb785e1af4b66bdb7
brunolnetto <brunolnetto@gmail.com>
2026-09-02 19:01:07 -0300

    refactor(): remove arquivos legados de template

--- data/deh-dbt-fundamentals/ecommerce/macros/normalize_email.sql
+++ data/deh-dbt-fundamentals/ecommerce/macros/normalize_email.sql
@@ -3,15 +3,3 @@
     lower(trim({{ column_name }}))
 
 {% endmacro %}
-
-{% macro amount_for_status(amount_column, status_column, expected_status) %}
-
-    case
-        when {{ status_column }} = '{{ expected_status }}'
-        then {{ amount_column }}
-        else 0
-    end
-
-{% endmacro %}
-
-
